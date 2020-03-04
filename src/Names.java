33402650c38b79dbac6b27ad2e34ac9262f5ca53
WBlijjenberg <wietske@blijjenberg.com>
2020-03-04 14:11:27 +0100

    Update Names.java
    More names.

--- src/Names.java
+++ src/Names.java
@@ -1,5 +1,8 @@
 public class Names {
-    public String[] male = {"Daan", "Jasper", "Job"};
-    public String[] female = {"Sanne", "Wietske"};
+    public String[] male = {"Daan", "Jasper", "Job", "Tim", "Martin", "Karim", 
+        "Max", "Ronald", "Ruben", "Derek", "Chad", "Ralph", "Pete", "Tom", "Thijs" };
+    public String[] female = {"Sanne", "Wietske", "Kim", "Lola", "Melissa", 
+        "Myrte", "Anna", "Chantal", "Louise", "Natasha", "Pauline", "Hannah", 
+        "Daphne", "Tamara", "Connie", "Barbara", "Petra", "Susan", "Susanna" };
     public String[][] names = {male, female};
 }
