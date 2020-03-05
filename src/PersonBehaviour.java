b4678b902c4c871003507b3f3b60d381bbbd95db
JasperDell <jasper.del@hotmail.com>
2020-03-05 14:59:53 +0100

    People now have a chance of buying alcohol when they have money

--- src/PersonBehaviour.java
+++ src/PersonBehaviour.java
@@ -1,10 +1,18 @@
 import java.util.List;
+import java.util.*;
 
 public class PersonBehaviour {
-
+    Random random = new Random();
     List<Person> updatePerson (List<Person> crowd) {
         for(Person p : crowd) {
             p.energy--;
+            int money = p.getMoneyToSpend();
+            if (money>=1){
+                if (random.nextBoolean()){
+                    p.updateDrinksConsumed(1);
+                    p.updateMoneyToSpend(-1);
+                }
+            }
         }
         return crowd;
     }
